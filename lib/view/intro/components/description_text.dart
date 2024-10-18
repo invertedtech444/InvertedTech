995a9e8719c4f446870406de13133e82e86ffc84
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 <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>
2024-10-18 12:58:04 +0500

    NEw Branch from dev-asad

--- lib/view/intro/components/description_text.dart
+++ lib/view/intro/components/description_text.dart
@@ -16,7 +16,7 @@ class AnimatedDescriptionText extends StatelessWidget {
         return Padding(
           padding: const EdgeInsets.only(top: 8.0),
           child: Text(
-            '   - Real-time detection and recognition of registered users.\n   - Compares captured facial data with stored records to \n   identify individuals.\n   - Supports various lighting conditions and environments \n   for robust recognition.\n   - Provides feedback to users during the recognition \n   process to ensure accuracy.\n   - Utilizes advanced algorithms to handle variations \n   in facial expressions and poses.\n   - Anti-spoofing technology prevents recognition through \n   photos or videos.   \n- Works entirely offline with no internet dependency, \n   ensuring secure and fast recognition.',
+            '   - Real-time detection and recognition of registered users.\n   - Compares captured facial data with stored records to \n   identify individuals.\n   - Supports various lighting conditions and environments \n   for robust recognition.\n   - Provides feedback to users during the recognition \n   process to ensure accuracy.\n   - Utilizes advanced algorithms to handle variations \n   in facial expressions and poses.\n   - Anti-spoofing technology prevents recognition through \n   photos or videos.\n   - Works entirely offline with no internet dependency, \n   ensuring secure and fast recognition.',
             maxLines: 20,
             overflow: TextOverflow.ellipsis,
             style: TextStyle(color: Colors.white, wordSpacing: 2, fontSize: value),
